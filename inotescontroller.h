bc2148023098ed7d1c1593efc5da3380d2ff4ba6
glbter <nicknema13@gmail.com>
2021-04-20 16:21:31 +0300

    fix: linker error

--- inotescontroller.h
+++ inotescontroller.h
@@ -14,5 +14,5 @@ public:
     virtual Note& AddNote(const string& title, const  string& text) = 0;
     virtual Note GetNote(const int& id) = 0;
     virtual vector<Note> GetAllNotes() = 0;
-    virtual bool Login(const string& login, const string& password);
+    virtual bool Login(const string& login, const string& password) = 0;
 };
